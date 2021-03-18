e44e121fd8152db4fbff3680126724e02ec37f0a
KMRH47 <kramze47@gmail.com>
2021-03-18 15:40:30 +0100

    removed webbuilder and runs at local IP with HTTP 80

--- VTourAPI/VTourAPI/Program.cs
+++ VTourAPI/VTourAPI/Program.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,14 +10,31 @@ namespace VTourAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+                .UseKestrel(
+                    options =>
+                    {
+                        options.AddServerHeader = false;
+                        options.Listen(IPAddress.Any, 5000, listenOptions =>
+                        {
+                           // listenOptions.UseHttps();
+                        });
+                    }
+                )
+                // .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                // .UseUrls("https://localhost:44321")
+                .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    //webBuilder.UseStartup<Startup>();
                 });
     }
 }
